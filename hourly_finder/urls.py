b4090864aa11d8e9611f6a397158bc9e78784d97
Rashed Ahmed <rashed.ahmed@email.com>
2022-03-08 12:30:37 +0600

    create auth urls

--- hourly_finder/urls.py
+++ hourly_finder/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('app_auth.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)

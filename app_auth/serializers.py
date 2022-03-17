2070cbfc8a1ef3c11f180d726fe1cfbcd89612f4
Rashed Ahmed <rashed.ahmed@email.com>
2022-03-18 02:58:59 +0600

    create profile during user creation

--- app_auth/serializers.py
+++ app_auth/serializers.py
@@ -1,7 +1,23 @@
 from djoser.serializers import UserCreateSerializer
+from app_user.models import SeekerProfile,RecruiterProfile
 from .models import User
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id','email','username','password','first_name','last_name','user_type')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+    
+    def create(self, validated_data):
+        user_type = validated_data.pop('user_type')
+        user = User.objects.create_user(**validated_data)
+        if user_type==1:
+            SeekerProfile.objects.create(user=user)
+        elif user_type==2:
+            RecruiterProfile.objects.create(user=user)        
+        return user
--- app_user/models.py
+++ app_user/models.py
@@ -5,15 +5,15 @@ from app_auth.models import User
 class RecruiterProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     bio = models.TextField()
-    phone = models.CharField(max_length=15)
-    designation = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15,blank=True)
+    designation = models.CharField(max_length=50,blank=True)
     def __str__(self):
         return self.user.first_name+ ' Recruiter'
 
 class SeekerProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     bio = models.TextField()
-    phone = models.CharField(max_length=15)
-    expertise = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15,blank=True)
+    expertise = models.CharField(max_length=50,blank=True)
     def __str__(self):
         return self.user.first_name+ ' Seeker'

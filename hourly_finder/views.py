bbfd3cad396e4d1a776241160b284cd714d11f75
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-04-19 02:27:05 +0600

    API Endpoints Documentation

--- hourly_finder/views.py
+++ hourly_finder/views.py
@@ -1,27 +1,78 @@
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.decorators import api_view,permission_classes
+
 
 @api_view(['GET'])
 @permission_classes([])
 def getRoutes(request):
     routes = [
         {
-            'Endpoint': '/auth',
-            'method': 'GET',
-            'body': None,
-            'description': 'returns authentication'
-        },
-        {
-            'Endpoint': '/user',
-            'method': 'GET',
-            'body': None,
-            'description': 'returns user information'
-        },
-        {
-            'Endpoint': '/jobs',
-            'method': 'GET',
-            'body': None,
-            'description': 'returns job information'
-        },
+            'Application' : 'HourlyFinder API',
+            'Feature List' : [
+                {
+                    "Name" : "Auth",
+                    "Endpoints" : [
+                        {
+                            'Endpoint': '/auth',
+                            'method': 'GET',
+                            'body': None,
+                            'description': 'returns authentication'
+                        },
+                        {
+                            'Endpoint': '/user',
+                            'method': 'GET',
+                            'body': None,
+                            'description': 'returns user information'
+                        },
+                        
+                    ]
+                }, 
+                {
+                    "Name" : "Job",
+                    "Endpoints" : [
+                        {
+                            'Endpoint': '/jobs',
+                            'method': 'GET',
+                            'body': None,
+                            'description': 'returns job information'
+                        },
+                    ]
+                },
+                {
+                    "Name" : "Blog",
+                    "Endpoints" : [
+                        {
+                            'Endpoint': '/blogs/?page_size=6',
+                            'method': 'GET',
+                            'body': None,
+                            'description': 'returns Blog information with pagination.'
+                        },
+                        {
+                            'Endpoint': '/blogs/{SlugId}',
+                            'method': 'GET',
+                            'body': None,
+                            'description': 'returns Single Blog Details information with pagination.'
+                        },
+                    ]
+                },
+                {
+                    "Name" : "Contact",
+                    "Endpoints" : [
+                        {
+                            'Endpoint': '/contact/create',
+                            'method': 'POST',
+                            'body': '{data}',
+                            'description': 'create a query from user and send mail to HourlyFinder Team.'
+                        },
+                        {
+                            'Endpoint': '/contact/newsletter',
+                            'method': 'POST',
+                            'body': '{email}',
+                            'description': 'Add Newsletter Subscription via Email and get confirmation mail.'
+                        }
+                    ]
+                }
+            ]
+        }
     ]
     return Response(routes)
